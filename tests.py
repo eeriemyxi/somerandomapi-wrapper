a54c6b54002e1be8515c1c3030fe48da282cdf64
Myxi <myxi@duck.com>
2021-12-08 16:56:22 +0530

    trying to remove trash

--- tests.py
+++ tests.py
@@ -1,19 +1,6 @@
 import somerandomapi
 import asyncio
 
-# with somerandomapi.Filter.greyscale(key="u9epAaOr8RrTCMle62hxnVCGf", avatar="https://cdn.discordapp.com/avatars/764897680060973067/f3593c937adabc2da8bb8185f85dc5e3.png?size=4096") as resp:
-#     print(type(resp))
-# with somerandomapi.Other.joke as r:
-#     print(r)
-    # print(type(r))
-    # with open("hi.png", "wb") as f: f.write(r); f.close()
-# async def main():
-#     async with somerandomapi.Welcome("u9epAaOr8RrTCMle62hxnVCGf", 1, "stars2", "join", "https://cdn.discordapp.com/avatars/764897680060973067/f3593c937adabc2da8bb8185f85dc5e3.png?size=4096", "Myxi", 4444, "Server Name", "red", 333) as resp:
-#         with open("hi.png", "wb") as f:
-#             f.write(resp)
-#             f.close()
-
-# asyncio.run(main())
 
 async def main():
     async with somerandomapi.Other.joke as r:
